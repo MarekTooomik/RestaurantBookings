8345ca2c9ce3c1098e5a6ef55bf1d7f8867eba97
MarekTooomik <marek.toomik@gmail.com>
2025-01-09 19:19:57 +0200

    Add project files.

---
+++ RestaurantBookings/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantBookings
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RestaurantBookings/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RestaurantBookings
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
